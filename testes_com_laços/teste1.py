06c5e8d0c1edc8dba3b1ca877374d8acf7ad6455
guilhermegit <g360guilherme@gmail.com>
2023-03-24 00:15:28 -0300

    codigo melhorado

--- testes_com_laços/teste1.py
+++ testes_com_laços/teste1.py
@@ -1,3 +1,7 @@
 nomes = ["ana", "MARIA", "JOaO", "Jose", "zeca", "Diego"]
 for nome in nomes:
     print(nome.lower())
+lista = list(range(1,6))
+print(lista)
+for num in range (1,6):
+    print(num * 100)
