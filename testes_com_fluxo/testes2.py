03ddfb07217c4ba29321c902bfbbf13c5ba17c03
guilhermegit <g360guilherme@gmail.com>
2023-03-23 23:46:13 -0300

    brincando e desenvolvendo com os ifs e elifs

---
+++ testes_com_fluxo/testes2.py
@@ -0,0 +1,12 @@
+import random as rd
+import random as rd
+d20 = rd.randint(1,20)
+if d20 == 1:
+    print("o player tirou uma falha critica")
+elif 1 < d20 < 11:
+    print("o player tirou uma falha")
+elif 10 < d20 < 20:
+    print("o player tirou um acerto")
+elif d20 == 20:
+    print("O PLAYER TIROU UM 20!!!! MINHA NOSSA")
+print(f"o numero tirado no d20 foi {d20}")
