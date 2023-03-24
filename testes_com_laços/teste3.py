701018d92a5c9132ade06fd823b8e49574175a8f
guilhermegit <g360guilherme@gmail.com>
2023-03-24 01:03:38 -0300

    mais testes

--- testes_com_laços/teste3.py
+++ testes_com_laços/teste3.py
@@ -5,3 +5,4 @@ idades = [20, 68, 25, 40, 60, 50]
 for indice, idade in enumerate(idades):
     if idade > 50:
         print(f"indice:{indice} contem idade maior que 50")
+print(idades[1], idades[4])
