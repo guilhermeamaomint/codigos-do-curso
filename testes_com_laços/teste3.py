9e3a49ca4faa3ff15a69103e9f7479b421395bc1
guilhermegit <g360guilherme@gmail.com>
2023-03-24 01:00:48 -0300

    mais testes

---
+++ testes_com_laços/teste3.py
@@ -0,0 +1,7 @@
+nomes = ["ana", "MARIA", "JOaO", "Jose", "zeca", "Diego"]
+idades = [20, 68, 25, 40, 60, 50]
+
+
+for indice, idade in enumerate(idades):
+    if idade > 50:
+        print(f"indice:{indice} contem idade maior que 50")
