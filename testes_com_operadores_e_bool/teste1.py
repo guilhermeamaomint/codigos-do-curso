84ccd450a550852a6de7a9a8560b4442e7bbabf3
guilhermegit <g360guilherme@gmail.com>
2023-03-27 11:27:01 -0300

    muitas alterações realizadas

---
+++ testes_com_operadores_e_bool/teste1.py
@@ -0,0 +1,9 @@
+prod_vendidos = 100
+prod_estoque = 90
+
+print(f"igual a (==). {prod_vendidos == prod_estoque}")
+print(f"diferente de (!=): {prod_vendidos != prod_estoque}")
+print(f"menor que (<); {prod_vendidos <prod_estoque}")
+print(f"menor ou igual a (<=): {prod_vendidos <= prod_estoque}")
+print(f"maior que (>): {prod_vendidos > prod_estoque}")
+print(f"maior ou igual a (>=): {prod_vendidos >= prod_estoque}")
