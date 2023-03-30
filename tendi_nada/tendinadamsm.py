1531f5128f77b03e29f5651bef62259fee49a38a
guilhermegit <g360guilherme@gmail.com>
2023-03-30 16:25:20 -0300

    praticando com exercicios

---
+++ tendi_nada/tendinadamsm.py
@@ -0,0 +1,8 @@
+numero1 = int(input("digite o primeiro numero: "))
+numero2 = int(input("digite o segundo numero: "))
+resultado1 = (numero1 * 2) * (numero2 / 2)
+print(f"o produto do dobro do primeiro numero pela metade do segundo é: {resultado1}")
+resultado2 = (numero1 * 3) + numero2
+print(f"a soma do triplo do primeiro numero com o segundo é: {resultado2}")
+resultado3 = (numero2 ** 3)
+print(f"o numero 2 elevado ao cubo é: {resultado3}")
