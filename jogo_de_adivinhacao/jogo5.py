a117f5b9662ecd07545883cf8747eb745f3fb132
guilhermegit <g360guilherme@gmail.com>
2023-03-30 15:29:46 -0300

    estudando

--- jogo_de_adivinhacao/jogo5.py
+++ jogo_de_adivinhacao/jogo5.py
@@ -1,14 +1,18 @@
 import random as rd
-numero = rd.randint(1, 10)
-tentativas = 5
+numero = rd.randint(1, 100)
+tentativas = 10
 while tentativas != 0:
     chute = int(input("digite seu chute: "))
     tentativas -= 1
-    if numero == chute:
+    diferenca = numero - chute
+    if diferenca == 0:
         print("Parabens, vc acertou!")
         break
-    elif numero > chute:
+    elif diferenca <= 5 and diferenca >= -5:
+        print("vc errou, mas esta perto")
+    elif diferenca > 0:
         print("vc errou, o numero era maior")
     else:
         print("vc errou, o numero era menor")
+print(numero)
 print("FIM DO PROGRAMA")
