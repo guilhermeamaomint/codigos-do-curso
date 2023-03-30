7ecd68eac494f156bba839d01bbf8cf7d9f61f63
guilhermegit <g360guilherme@gmail.com>
2023-03-30 16:08:23 -0300

    teste 2 atualizado

--- simulador_de_dados/teste2.py
+++ simulador_de_dados/teste2.py
@@ -1,13 +1,15 @@
 import random as rd
 
 quantidade = int(input("quantos dados vc quer rolar?: "))
-total = 0
+valores = []
 rolagem = 1
 while rolagem <= quantidade:
     valor = rd.randint(1, 6)
-    print(valor)
+    valores.append(valor)
     rolagem += 1
-    total = total + valor
-print(f"O total é: {total}")
-print(f"A media é: {total / quantidade}")
+print(valores)
+print("o total é:", sum(valores))
+print("a media é:", sum(valores) / quantidade)
+print("maior: ", max(valores))
+print("menor: ", min(valores))
 print("FIM DO PROGRAMA")
