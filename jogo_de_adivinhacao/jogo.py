bfb846db99ebe65f4e2a4bc4d11ba7e3d299b8b3
guilhermegit <g360guilherme@gmail.com>
2023-03-30 15:00:55 -0300

    estudando

--- jogo_de_adivinhacao/jogo.py
+++ jogo_de_adivinhacao/jogo.py
@@ -2,8 +2,8 @@ import random as rd
 
 numero = rd.randint(1, 3)
 
-chute = input("digite seu chute: ")
-chute = int(chute)
+chute = int(input("digite seu chute: "))
+
 print(numero == chute)
 
 print(numero)
